8cbffee08f787d53a0c28879faabb23a865dd8be
Michael Fleet <disinnovate@gmail.com>
2009-07-17 01:01:58 -0400

    preparing sprout library

--- rakefile.rb
+++ rakefile.rb
@@ -54,3 +54,25 @@ swc :swc
 
 # set up the default rake task
 task :default => :debug
+
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "sprout-as3spec-library"
+    gem.summary = %Q{AS3Spec is a tiny BDD framework for AS3, inspired by Bacon and RSpec. Built upon sprout project-generation tool for ActionScript/Flash/Flex/AIR.}
+    gem.description = %Q{AS3Spec is a tiny BDD framework for AS3, inspired by Bacon and RSpec. Built upon sprout project-generation tool for ActionScript/Flash/Flex/AIR.}
+    gem.email = "disinnovate@gmail.com"
+    gem.homepage = "http://github.com/disinnovate/as3spec"
+    gem.authors = ["Michael Fleet"]
+		gem.executables = []
+		gem.files = FileList['sprout.spec']
+		gem.extra_rdoc_files = []
+		gem.require_path = '.'
+#		gem.add_dependency('sprout', '>= 0.7.206')
+    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
+  end
+
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+end
---
+++ sprout.spec
@@ -0,0 +1,4 @@
+- !ruby/object:Sprout::RemoteFileTarget
+  platform: universal
+  url: http://cloud.github.com/downloads/disinnovate/as3spec/as3spec.swc.zip
+  archive_path: 'as3spec.swc'
