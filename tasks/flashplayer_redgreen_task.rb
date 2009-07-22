be8af4ab09fea5e3e52c188ebaeedbaf25061e49
Michael Fleet <disinnovate@gmail.com>
2009-07-21 22:57:36 -0400

    win32 color output, untested

--- tasks/flashplayer_redgreen_task.rb
+++ tasks/flashplayer_redgreen_task.rb
@@ -2,11 +2,26 @@ module Sprout
   class FlashPlayerTask < Rake::Task
 			COLORS = { 'F' => 31,	'E' => 35, 'M' => 33, :ok => 32	}
 
+			# thank you rspec for win32 support ;)
+			# http://rspec.info/
+			COLORIZE = true
+			if RUBY_PLATFORM =~ /mswin|mingw/ ;\
+			  begin ;\
+			    require 'rubygems' unless ENV['NO_RUBYGEMS'] ;\
+			    require 'Win32/Console/ANSI' ;\
+				 	COLORIZE = true
+			  rescue LoadError ;\
+			    warn "You must 'gem install win32console' to use colour on Windows" ;\
+					COLORIZE = false
+			  end
+			end
+
 			def colorize_string(text, color); "\e[1m\e[#{COLORS[color]}m#{text}\e[0m"; end
 
 			def colorize_result(text)
 				out = text.sub('[trace] [as3spec]', '[as3spec]')
-				return out if PLATFORM =~ /win32/ 
+				# return out if PLATFORM =~ /win32/ 
+				return out unless COLORIZE
 
 				requirement_color = case out
 				when /(FAILED|ERROR|MISSING)/ then $1[0..0] # SpecDox, Tap, Knock
